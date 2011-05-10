1481987e44623fce088c84b6d52c3fc8f5d4b2b1
Graham Batty <graham-storm-git@ript.net>
2011-05-10 08:08:29 -0600

    Moved logging functionality from example to middleware.

--- example/log.rb
+++ example/log.rb
@@ -1,22 +1,16 @@
 # System whereby requests are logged to a log url as they come in.
 
 require 'sack'
+require 'sack/middleware/log_url'
 server = Sack.server('thin').new(:Host=>'localhost', :Port =>1025)
-log_reqs = []
+server = Sack::Middleware::LogUrl.new(server, '/log')
 
 Thread.new do
 	while req = server.wait
-		begin			
-			log_reqs.each do |log_req|
-				log_req << "#{req.env['HTTP_HOST']} - - #{Time.now.ctime} \"#{req.env['REQUEST_METHOD']} #{req.env['REQUEST_URI']} #{req.env['SERVER_PROTOCOL']}\" 200 -\n"
-			end
+		begin
 			body = req.headers(200, "Content-Type" => "text/html")
-			if (req.env['REQUEST_URI'] =~ %r{^/log})
-				log_reqs.push(body)
-			else
-				body << "Oh hai thar"
-				body.done
-			end
+			body << "Oh hai thar, #{req.env["REQUEST_URI"]}"
+			body.done
 		rescue Object => e
 			puts(e)
 		end
---
+++ lib/sack/middleware/log_url.rb
@@ -0,0 +1,74 @@
+module Sack
+	module Middleware
+		class LogUrl
+
+			class Request
+				class Body
+					def initialize(real_body, status, env, log_reqs, time)
+						@real_body = real_body
+						@status = status
+						@env = env
+						@log_reqs = log_reqs
+						@time = time
+						@bytes = 0
+					end
+					def <<(str)
+						@bytes += str.length
+						@real_body << str
+					end
+					def done
+						@real_body.done
+						@log_reqs.each do |log_req|
+							begin
+								log_req << "#{@env['HTTP_HOST']} - - [#{@time}] \"#{@env['REQUEST_METHOD']} #{@env['REQUEST_URI']} #{@env['SERVER_PROTOCOL']}\" #{@status} #{@bytes}\n"
+							rescue # error writing to log output, we should remove it.
+							end
+						end
+					end
+				end
+
+				def initialize(real_req, log_reqs, time)
+					@real_req = real_req
+					@log_reqs = log_reqs
+					@time = time
+				end
+
+				def env; @real_req.env; end
+
+				def headers(status, headers)
+					Body.new(@real_req.headers(status, headers), status, env, @log_reqs, @time)
+				end
+			end
+
+			def initialize(parent, url)
+				@parent = parent
+				@url = url
+				@log_reqs = []
+			end
+
+			def wait
+				req = nil
+				while req.nil?
+					req = @parent.wait
+					if (req.env["REQUEST_URI"] == @url)
+						@log_reqs.push(req.headers(200, "Content-Type"=>"text/html"))
+						req = nil # don't pass out log requests.
+					else
+						log_req = Request.new(req, @log_reqs, Time.now)
+						req = log_req
+					end
+				end
+				return req
+			end
+			def start
+				@parent.start
+			end
+			def stop
+				@parent.stop
+			end
+			def stop!
+				@parent.stop!
+			end
+		end
+	end
+end
