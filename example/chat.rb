2acd1995f4acd587ec176654fcd6f93b9a61cc37
Graham Batty <graham-storm-git@ript.net>
2011-05-17 23:33:23 -0600

    Replacing dumb chat example with ES-based one.

--- example/chat.rb
+++ example/chat.rb
@@ -1,10 +1,57 @@
 # Simple web chat server
 
 require 'sack'
+require 'sack/connection_list'
 require 'rack/request'
-require 'sack/middleware/log_url'
-server = Sack.server('simple').new(:Host=>'localhost', :Port =>1025)
-chat_clients = []
+host = 'localhost'
+server = Sack.server('simple').new(:Host=>host, :Port =>1025)
+chat_clients = Sack::ConnectionList.new
+
+Page = <<DOC
+<html>
+<head>
+<script src="http://www.lateststate.com/polyfills/EventSource.js"></script>
+<script>
+function initStream() {
+  var stream = new EventSource("http://#{host}:1025/stream");
+  stream.onmessage = function(event) {
+    var chatArea = document.getElementById("chat");
+    var info = event.data.split("\\n");
+    var line = document.createElement('div');
+    var name = document.createElement('em');
+    name.appendChild(document.createTextNode("<" + info[0] + ">"));
+    line.appendChild(name);
+    line.appendChild(document.createTextNode(" " + info[1]));
+    chatArea.insertBefore(line, chatArea.firstChild);
+  };
+  stream.onerror = function(event) {
+    var timer = setTimeout(function() { clearTimeout(timer); initStream(); }, 40); // TODO: implement backoff
+  };
+};
+
+function submitText(form) {
+  var text = document.getElementById('text');
+  if (text.value != "") {
+    form.submit();
+    text.value = '';
+  }
+  return false;
+};
+</script>
+</head>
+<body onload="initStream();">
+  <iframe name="boom" style="display:none;"></iframe>
+  <div>
+    <form target="boom" method="post" action="/">
+      <input type="text" name="name" value="nickname" size="15"> 
+      <input type="text" name="text" id="text" value=""> 
+      <input type="submit" onclick="return submitText(this.form);">
+    </form>
+  </div>
+  <hr>
+  <div id="chat"></div>
+</body>
+DOC
 
 Thread.new do
   while req = server.wait
@@ -14,16 +61,18 @@ Thread.new do
         name = rack_req.params["name"]
         text = rack_req.params["text"]
 
-        chat_clients.each do |client|
-          client << "<div><em>&lt;#{name}&gt;</em> #{text}</div>\n"
-        end
         req.headers(200, "Content-Type" => "text/html").done
-      else
-        body = req.headers(200, "Content-Type" => "text/html")
-        body << '<iframe name="boom" width="0" height="0"></iframe>'
-        body << '<div><form target="boom" method="post" action="/"><input type="text" name="name" value="nickname" size="15"> <input type="text" name="text" value=""> <input type="submit"></div><hr>'
-        body << "        "*1024
+        chat_clients.each do |client|
+          client << "data: #{name}\ndata: #{text}\n\n"
+        end
+      elsif (req.env["REQUEST_URI"] == "/stream")
+        body = req.headers(200, "Content-Type" => "text/event-stream")
+        body << "retry: 1\n"
         chat_clients.push(body)
+      else
+        body = req.headers(200, "Content-Type" => "text/html", "Content-Length" => Page.length.to_s)
+        body << Page
+        body.done
       end
     rescue Object => e
       puts(e)
